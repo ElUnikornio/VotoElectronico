a8dc7c8d8dc01413809022ff9288a017ba5aa57f
ElUnikornio <gtorresbotello@gmail.com>
2021-06-04 11:02:46 -0500

    Añadí las principales estructuras de archivos y un retorno entero a la funcion admin

--- lib.h
+++ lib.h
@@ -1,8 +1,34 @@
+//Estructuras
+struct Personas
+{
+    char nombre[10];
+    char apellidoP[10];
+    char apellidoM[10];
+    int edad;
+    char nacionalidad[15];
+    char contrasena[10];
+    char voto;
+    char antecedentes;
+};
+
+struct Administrador
+{
+    char ususario[20];
+    char contrasena[10];
+};
+
+struct Candidato
+{
+    char partido[10];
+    char nombre[25];
+    int votos;
+};
+
 //  prototipos de funciones
-void admin();
+int admin();
 
 // Funciones
-void admin()
+int admin()
 {
     system("CLS");
     printf("SISTEMA DE VOTO ELECTR%cNICO", 224);
@@ -14,4 +40,7 @@ void admin()
     printf("\n\t6)Cerrar votaciones.");
     printf("\n\t7)Cambiar clave.");
     printf("\n\t8)Salir.");
+
+    fflush(stdin);
+    return getchar();
 }
