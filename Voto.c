66c7a294b792807887ce371299caf68c4f83f8de
ElUnikornio <gtorresbotello@gmail.com>
2021-06-08 20:25:00 -0500

    Plantilla para comenzar a trabajar en equipo

--- Voto.c
+++ Voto.c
@@ -4,43 +4,48 @@
 
 int main()
 {
-    int op;
-    char VotacionActiva = 'n';
-
-    printf("\nSelecciona una opci%cn", 162);
-    printf("\n\ta)Soy administrador");
-    printf("\n\tb)Soy votante");
-    printf("\n\ts)Salir\n");
-
-    switch (getchar())
+    comprobacionArchivos();
+    char VotacionActiva = 'n', op;
+    do
     {
-    case 'a':
-        printf("\tadmin\n");
-        admin();
-        break;
+        //system("CLS");
+        printf("\nSISTEMA DE VOTO ELECTR%cNICO", 224);
+        printf("\nSelecciona una opci%cn", 162);
+        printf("\n\ta)Soy administrador");
+        printf("\n\tb)Soy votante");
+        printf("\n\ts)Salir\n");
+        fflush(stdin);
+        scanf("%c", &op);
 
-    case 'b':
-        printf("votante\n");
-        if (VotacionActiva == 's')
+        switch (op)
         {
-            /* code */
+        case 'a':
+            //printf("\tadmin\n");
+            admin(&VotacionActiva);
+            break;
+
+        case 'b':
+            printf("votante\n");
+            if (VotacionActiva == 's')
+            {
+                /* code */
+            }
+            else
+            {
+                printf("Las votaciones a%cn no est%cn habilitadas\nEl administrador debe habilitarlas.", 163, 160);
+            }
+
+            break;
+
+        case 's':
+            printf("Adios");
+            break;
+
+        default:
+            printf("Usa una opci%cn v%clida", 162, 160);
+
+            break;
         }
-        else
-        {
-            printf("Las votaciones a%cn no est%cn habilitadas\nEl administrador debe habilitarlas.", 163, 160);
-        }
-
-        break;
-
-    case 's':
-        printf("Adios");
-        op = -1;
-        break;
-
-    default:
-        printf("Usa una opci%cn v%clida", 162, 160);
-
-        break;
-    }
+    } while (op != 's');
     return 0;
 }
--- lib.h
+++ lib.h
@@ -1,46 +1,119 @@
 //Estructuras
 struct Personas
 {
-    char nombre[10];
-    char apellidoP[10];
-    char apellidoM[10];
+    char nombre[10],
+        apellidoP[10],
+        apellidoM[10],
+        nacionalidad[15],
+        contrasena[10],
+        voto,
+        antecedentes;
     int edad;
-    char nacionalidad[15];
-    char contrasena[10];
-    char voto;
-    char antecedentes;
 };
 
 struct Administrador
 {
-    char ususario[20];
-    char contrasena[10];
+    char ususario[20],
+        contrasena[10];
 };
 
 struct Candidato
 {
-    char partido[10];
-    char nombre[25];
+    char partido[10],
+        nombre[25];
     int votos;
 };
 
+//  Define
+#define rutaVotantes "Votantes.txt"
+#define rutaAdmin "Admin.txt"
+#define rutaCandidato "Candidato.txt"
+
 //  prototipos de funciones
-int admin();
+void admin(int **Votacion);
+void comprobacionArchivos();
 
 // Funciones
-int admin()
+void comprobacionArchivos()
 {
-    system("CLS");
-    printf("SISTEMA DE VOTO ELECTR%cNICO", 224);
-    printf("\n\t1)Registrar votante.");
-    printf("\n\t2)Ver lista nominal de nombres.");
-    printf("\n\t3)Ver resultados de las elecciones.");
-    printf("\n\t4)Registrar candidato.");
-    printf("\n\t5)Abrir votaciones.");
-    printf("\n\t6)Cerrar votaciones.");
-    printf("\n\t7)Cambiar clave.");
-    printf("\n\t8)Salir.");
+    FILE *f;
 
-    fflush(stdin);
-    return getchar();
+    f = fopen(rutaVotantes, "r");
+    if (f == NULL)
+    {
+        f = fopen(rutaVotantes, "w");
+    }
+
+    f = fopen(rutaAdmin, "r");
+    if (f == NULL)
+    {
+        f = fopen(rutaAdmin, "w");
+    }
+
+    f = fopen(rutaCandidato, "r");
+    if (f == NULL)
+    {
+        f = fopen(rutaCandidato, "w");
+    }
+}
+
+void admin(int **Votacion)
+{
+    int op;
+
+    do
+    {
+        //system("CLS");
+        printf("\nSISTEMA DE VOTO ELECTR%cNICO", 224);
+        printf("\n\t1)Registrar votante.");
+        printf("\n\t2)Ver lista nominal de nombres.");
+        printf("\n\t3)Ver resultados de las elecciones.");
+        printf("\n\t4)Registrar candidato.");
+        printf("\n\t5)Abrir votaciones.");
+        printf("\n\t6)Cerrar votaciones.");
+        printf("\n\t7)Cambiar clave.");
+        printf("\n\t8)Salir.\n");
+        scanf("%i", &op);
+
+        switch (op)
+        {
+        case 1:
+            printf("\n\t1)Registrar votante.");
+            break;
+
+        case 2:
+            printf("\n\t2)Ver lista nominal de nombres.");
+            break;
+
+        case 3:
+            printf("\n\t3)Ver resultados de las elecciones.");
+            break;
+
+        case 4:
+            printf("\n\t4)Registrar candidato.");
+            break;
+
+        case 5:
+            printf("\n\t5)Abrir votaciones.");
+            *Votacion = 's';
+            break;
+
+        case 6:
+            printf("\n\t6)Cerrar votaciones.");
+            *Votacion = 'n';
+            break;
+
+        case 7:
+            printf("\n\t7)Cambiar clave.");
+            break;
+
+        case 8:
+            printf("\n\t8)Salir.\n");
+            break;
+
+        default:
+            printf("Usa una opci%cn v%clida", 162, 160);
+            break;
+        }
+    } while (op != 8);
 }
